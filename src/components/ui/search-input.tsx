234577687886efc8e98a681fae415d4a067c81d3
mtguerson <mtguerson@gmail.com>
2025-10-09 15:37:58 -0400

    feat: add loading state when searching

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -1,18 +1,28 @@
 'use client';
 
-import { SearchIcon } from 'lucide-react';
+import { Loader2, SearchIcon } from 'lucide-react';
 
 import { Input } from '@/components/ui/input';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 export function SearchInput() {
+  const [loading, setLoading] = useState(false);
+
   const searchParams = useSearchParams();
   const router = useRouter();
 
+  useEffect(() => {
+    setLoading(false);
+  }, [searchParams]);
+
   function handleSearch(e: React.FormEvent<HTMLFormElement>) {
+    setLoading(true);
     e.preventDefault();
+
     const formData = new FormData(e.target as HTMLFormElement);
     const search = formData.get('search');
+
     router.push(`/search?q=${search}`);
   }
 
@@ -29,9 +39,14 @@ export function SearchInput() {
             type="text"
             autoComplete="off"
             name="search"
+            disabled={loading}
           />
           <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
-            <SearchIcon size={16} />
+            {loading ? (
+              <Loader2 size={16} className="animate-spin" />
+            ) : (
+              <SearchIcon size={16} />
+            )}
           </div>
         </div>
       </div>
